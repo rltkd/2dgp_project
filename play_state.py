63bf54e19dc8d85ebdc20ec98dbea97f784a1ef1
rltkd <rltkd0101@gmail.com>
2022-10-18 11:40:39 +0900

    맵 시야 처리

--- play_state.py
+++ play_state.py
@@ -1,6 +1,12 @@
 from pico2d import *
 import game_framework
 width, height = 1024, 684
+
+character = None
+map = None
+sight = True
+running = True
+
 class Map:
     def __init__(self):
         self.image = load_image('stage.png')
@@ -8,14 +14,27 @@ class Map:
     def draw(self):
         self.image.draw(width//2, height//2)
 
+
+# class Sight:
+#     def __init__(self):
+#         self.back_x = width//5
+#         self.back_y = height//5
+#         self.image = load_image('back.png')
+#
+#     def update(self):
+#         pass
+#     def draw(self):
+#         self.image.draw(self.back_x, self.back_y)
 class Character:
     def __init__(self):
         self.x, self.y, self.z = 90, 90, 4
         self.frame = 0
         self.dir_x, self.dir_y = 0, 0
         self.image = load_image('character_animation.png')
-
+        self.sight_image = load_image('back.png')
+        self.screen_delay=0
     def update(self):
+        global sight
         self.frame = (self.frame + 1) % 8
         self.x += self.dir_x*5
         self.y += self.dir_y*5
@@ -27,18 +46,23 @@ class Character:
             self.y = height
         elif self.y < 0:
             self.y =0
+
+        if not sight:
+            self.screen_delay=(self.screen_delay+1)%10
+            if(self.screen_delay == 0):
+                sight = True
     def draw(self):
+        if sight == True:
+            self.sight_image.draw(self.x, self.y-90)
 
         self.image.clip_draw(self.frame*100, 100*self.z, 100, 100, self.x, self.y)
 
 
 
-character = None
-map = None
-running = True
+
 
 def handle_events():
-    global running
+    global running, sight
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -49,15 +73,21 @@ def handle_events():
             if event.key == SDLK_RIGHT:
                 character.z=0
                 character.dir_x += 1
+
             elif event.key == SDLK_LEFT:
                 character.z = 1
                 character.dir_x -= 1
+
             elif event.key == SDLK_UP:
                 character.z = 2
                 character.dir_y += 1
+
             elif event.key == SDLK_DOWN:
                 character.z = 3
                 character.dir_y -= 1
+
+            elif event. key == SDLK_1:
+                sight = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 character.z= 4
@@ -73,9 +103,10 @@ def handle_events():
                 character.dir_y += 1
 
 def enter():
-    global character, map, running
+    global character, map,  running
     character = Character()
     map = Map()
+
     running = True
 
 #게임 종료 - 객체 소멸
@@ -89,7 +120,9 @@ def update():
     character.update()
 def draw_world():
     map.draw()
+    # sight.draw()
     character.draw()
+
 
 
 def draw():
