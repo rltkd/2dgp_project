7956a86548ef8281e65b7baf29cefe3128fa9d5e
rltkd <rltkd0101@gmail.com>
2022-12-01 11:32:03 +0900

    Update MAP.py

--- MAP.py
+++ MAP.py
@@ -21,12 +21,16 @@ class Block:
         # self.image = load_image('stage.png')
         rows = 16*60
         cols = 13*60
+        self.x, self.y = x, y
 
         for cols in range(len(map_data)):
             for rows in range(len(map_data[cols])):
                 pass
 
-                if
+                if map_data[cols][rows] == 1:
+                    self.image = load_image('block.png')
+                if map_data[cols][rows] ==0:
+                    pass
 
 
 
